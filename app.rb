9c52e53802588936053e4cb038790f5bf4c9a51a
camclark02 <164952709+camclark02@users.noreply.github.com>
2024-04-16 07:14:37 +0000

    starter code

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
   "
@@ -7,16 +8,16 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
   # build the API url, including the API key in the query string
-  api_url = "https://api.exchangerate.host/list?access_key=EXCHANGE_RATE_KEY"
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
 
   # use HTTP.get to retrieve the API information
   raw_data = HTTP.get(api_url)
 
   # convert the raw request to a string
   raw_data_string = raw_data.to_s
-
+  
   # convert the string to JSON
-  parsed_data = JSON.parse(raw_data_string)
+    parsed_data = JSON.parse(raw_data_string)
 
   # get the symbols from the JSON
   # @symbols = parsed_data ...
