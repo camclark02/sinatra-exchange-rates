2fa03f4a20e5aae1e9e1bc8ec8e18f102be75d13
camclark02 <164952709+camclark02@users.noreply.github.com>
2024-04-16 18:36:54 +0000

    everything up to the final route done

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require "sinatra"
 require "sinatra/reloader"
 require "http"
+require "json"
 
 get("/") do
   "
@@ -17,22 +18,32 @@ get("/") do
   raw_data_string = raw_data.to_s
   # pp raw_data_string
   # convert the string to JSON
-    @parsed_data = JSON.parse(raw_data_string)
+  @parsed_data = JSON.parse(raw_data_string)
 
   # get the symbols from the JSON
-    @symbols = @parsed_data.fetch("currencies").keys
+  @symbols = @parsed_data.fetch("currencies").keys
 
   # render a view template where I show the symbols
-   erb(:homepage)
+  erb(:homepage)
 end
 # ...
-
 get("/:from_currency") do
   @original_currency = params.fetch("from_currency")
 
   api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
-  
-  # some more code to parse the URL and render a view template
+
+  raw_data = HTTP.get(api_url)
+
+  # convert the raw request to a string
+  raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  @parsed_data = JSON.parse(raw_data_string)
+
+  # get the symbols from the JSON
+  @symbols = @parsed_data.fetch("currencies").keys
+
+  erb (:currency)
 end
 
 get("/:from_currency/:to_currency") do
@@ -40,6 +51,17 @@ get("/:from_currency/:to_currency") do
   @destination_currency = params.fetch("to_currency")
 
   api_url = "https://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
-  
-  # some more code to parse the URL and render a view template
+
+  raw_data = HTTP.get(api_url)
+
+  # convert the raw request to a string
+  raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  @parsed_data = JSON.parse(raw_data_string)
+
+  # get the symbols from the JSON
+  @rate = @parsed_data.fetch("result")
+
+  erb(:exchange)
 end
